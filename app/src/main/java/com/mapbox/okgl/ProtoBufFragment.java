8d4076a7bac459c9708606d5bff5f0ef60622b4e
Brad Leege <bleege@gmail.com>
2015-02-18 19:48:56 -0600

    Building ProtoBuf UI

--- app/src/main/java/com/mapbox/okgl/ProtoBufFragment.java
+++ app/src/main/java/com/mapbox/okgl/ProtoBufFragment.java
@@ -6,12 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class ProtoBufFragment extends Fragment {
 
+    private static final String TAG = "ProtoBufFragment";
+
+    private ProgressBar progressBar;
+    private Button controlButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_protobuf, container, false);
+        View view = inflater.inflate(R.layout.fragment_protobuf, container, false);
+
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        controlButton = (Button) view.findViewById(R.id.controlButton);
+
+        return view;
     }
 
 }
